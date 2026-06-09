cbeb1db03452e389d06cfe3ddaa592645659944a
Caíque Felix <caique.fm@gmail.com>
2026-06-09 15:47:39 -0300

    feat: add age calculator exercise

--- src/inputoutput/AgeCalculator.java
+++ src/inputoutput/AgeCalculator.java
@@ -1,4 +1,24 @@
 package inputoutput;
 
-public class Exercise01 {
+import java.util.Scanner;
+
+public class AgeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bom dia! \n" +
+                "Por favor, informe seu nome: ");
+        var name = scanner.nextLine();
+
+        System.out.println("Agora informe o ano do seu nascimento: ");
+        var yearBorn = scanner.nextInt();
+
+        var actualYear = java.time.Year.now().getValue();
+
+        var age = actualYear - yearBorn;
+
+        System.out.printf("\nOlá %s, você tem %d anos!", name, age);
+
+        scanner.close();
+    }
 }
