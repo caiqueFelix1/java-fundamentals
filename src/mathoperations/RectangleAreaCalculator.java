fa52c080cf1526d39460ec185c7d222c524dedf6
Caíque Felix <caique.fm@gmail.com>
2026-06-10 10:37:52 -0300

    feat: add rectangle area calculator using Scanner input

--- src/mathoperations/RectangleAreaCalculator.java
+++ src/mathoperations/RectangleAreaCalculator.java
@@ -1,4 +1,21 @@
 package mathoperations;
 
-public class Exercise03 {
+import java.util.Scanner;
+
+public class RectangleAreaCalculator{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro lado a ser calculado: ");
+        double firstSide = scanner.nextDouble();
+
+        System.out.println("Digite o segundo lado a ser calculado: ");
+        double secondSide =  scanner.nextDouble();
+
+        double area = firstSide * secondSide;
+
+        System.out.printf("A área do retângulo é: %.2f", area);
+
+        scanner.close();
+    }
 }
