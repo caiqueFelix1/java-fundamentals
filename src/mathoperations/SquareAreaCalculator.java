aa35977e7d27671392e7f64c7b0d56ca6007c145
Caíque Felix <caique.fm@gmail.com>
2026-06-15 12:39:32 -0300

    feat: implement square area calculator exercise

--- src/mathoperations/SquareAreaCalculator.java
+++ src/mathoperations/SquareAreaCalculator.java
@@ -1,4 +1,17 @@
 package mathoperations;
 
-public class Exercise02 {
+import java.util.Scanner;
+
+public class SquareAreaCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final String GREETING = "Olá!";
+
+        System.out.println(GREETING + "\nDigite o tamanho do lado do quadrado (em metros): ");
+        var side = scanner.nextDouble();
+
+        var result = side * side;
+        System.out.println(result);
+    }
 }
